11e5960a63ab7bf6f960a61e4eb16df1b2d8e99d
Elmo de Moraes <eomoraes@tjma.jus.br>
2025-01-29 19:01:55 -0300

    ajustes script auto pull

--- scripts/auto-pull.mjs
+++ scripts/auto-pull.mjs
@@ -1,17 +1,30 @@
 import { exec } from 'child_process'
 
 console.log('====== j2-modelos/modelos ======')
-console.log('Executando git pull...')
+console.log('Descartando alterações locais e executando git pull...')
 
-exec('git pull origin master', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Erro ao executar git pull: ${error.message}`)
+// Primeiro, rodar o comando para descartar alterações locais
+exec('git reset --hard HEAD', (resetError, resetStdout, resetStderr) => {
+  if (resetError) {
+    console.error(`Erro ao resetar mudanças locais: ${resetError.message}`)
     return
   }
-  if (stderr) {
-    console.error(`stderr: ${stderr}`)
+  if (resetStderr) {
+    console.error(`stderr (reset): ${resetStderr}`)
   }
-  console.log(`stdout: ${stdout}`)
+  console.log(`stdout (reset): ${resetStdout}`)
+
+  // Agora que as mudanças locais foram descartadas, executa o git pull
+  exec('git pull origin master', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Erro ao executar git pull: ${error.message}`)
+      return
+    }
+    if (stderr) {
+      console.error(`stderr: ${stderr}`)
+    }
+    console.log(`stdout: ${stdout}`)
+  })
 })
 
-console.log('timstamp: ', new Date())
+console.log('timestamp: ', new Date())
